e35351423f1da36ca5f03043ed94d30c33607d53
hostedposted <hostedpostedsite@gmail.com>
2022-11-06 21:02:57 -0500

    Tokenify Fix

--- lib/tokenify.d.ts
+++ lib/tokenify.d.ts
@@ -1,29 +1,23 @@
-export declare const tokenify: (username: string, password: string, { log }?: {
-    log?: boolean | undefined;
-}) => Promise<{
+interface MasterResponse {
+    clientID: string;
+    userID: number;
+    token: string;
     authToken: string;
-    classIDs: number[];
-    curriculumOverride: unknown;
-    curriculumTreeID: number;
-    goalId: unknown;
-    grade: number;
-    isMember: 0 | 1;
-    isTowerTownEnabled: boolean;
-    lastVisited: string;
-    memberEndDate: string | null;
-    memberStartDate: string | null;
+    userType: string;
     name: string;
     objectID: number;
-    ownerIDs: number[];
-    parentEmail: string | null;
-    placementTestID: number;
     registerDate: string;
-    token: string;
-    userID: number;
-    usertype: string;
-    expires_in: string;
-    state: string;
-    id_token: string;
-    access_token: string;
-    token_type: "Bearer";
-}>;
+    lastVisited: string;
+    ownerIDs: number[];
+    classIDs: number[];
+    isTowerTownEnabled: boolean;
+    identityUri: string;
+    identityClientId: string;
+    identityToken: string;
+    refreshToken: string;
+    premises: string;
+}
+export declare const tokenify: (username: string, password: string, { log }?: {
+    log?: boolean | undefined;
+}) => Promise<MasterResponse>;
+export {};
--- src/tokenify.ts
+++ src/tokenify.ts
@@ -7,36 +7,27 @@ if (typeof SharedArrayBuffer === "undefined") {
 import JSDOM from "jsdom";
 import fetch from "node-fetch";
 import fetchCookie from "fetch-cookie";
+import { sha256 } from "js-sha256";
 import { URL } from "url";
 
-interface TokenResponse {
-    expires_in: string,
-    state: string,
-    id_token: string,
-    access_token: string,
-    token_type: "Bearer"
-}
 interface MasterResponse {
-    authToken: string;
-    classIDs: number[];
-    curriculumOverride: unknown;
-    curriculumTreeID: number;
-    goalId: unknown;
-    grade: number;
-    isMember: 0 | 1;
-    isTowerTownEnabled: boolean;
-    lastVisited: string;
-    memberEndDate: string | null;
-    memberStartDate: string | null
-    name: string;
-    objectID: number;
+    clientID: string
+    userID: number
+    token: string
+    authToken: string
+    userType: string
+    name: string
+    objectID: number
+    registerDate: string
+    lastVisited: string
     ownerIDs: number[]
-    parentEmail: string | null;
-    placementTestID: number
-    registerDate: string;
-    token: string;
-    userID: number;
-    usertype: string;
+    classIDs: number[]
+    isTowerTownEnabled: boolean
+    identityUri: string
+    identityClientId: string
+    identityToken: string
+    refreshToken: string
+    premises: string
 }
 
 const fromEntries = <T = any>(iterable: Iterable<readonly [PropertyKey, T]>) =>
@@ -44,6 +35,8 @@ const fromEntries = <T = any>(iterable: Iterable<readonly [PropertyKey, T]>) =>
         obj[key as string] = val;
         return obj;
     }, {} as { [key: string]: T });
+
+const genRanHex = (size: number) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
 
 export const tokenify = async (username: string, password: string, { log }: { log?: boolean } = {}) => {
     const cookiefetch: typeof fetch = fetchCookie(fetch);
@@ -79,50 +72,112 @@ export const tokenify = async (username: string, password: string, { log }: { lo
     if (log) console.log(`Initial login request done with a code of ${login.status}.`);
     const playLoginParams = new URLSearchParams();
     playLoginParams.set("authenticity_token", new JSDOM.JSDOM(await (await cookiefetch(login.headers.get("location") ?? "")).text()).window.document.querySelector("input[name=authenticity_token]")?.getAttribute("value") ?? "");
-    const schoolLogin = await cookiefetch(`https://sso.prodigygame.com/premises?premises=home&rid=${new URL(login.headers.get("location") || "").searchParams.get("rid")}`, { headers: { "content-type": "application/x-www-form-urlencoded" }, body: playLoginParams.toString(), method: "POST", redirect: "manual" });
+    const rid = new URL(login.headers.get("location") || "").searchParams.get("rid");
+    const schoolLogin = await cookiefetch(`https://sso.prodigygame.com/premises?premises=home&rid=${rid}`, { headers: { "content-type": "application/x-www-form-urlencoded" }, body: playLoginParams.toString(), method: "POST", redirect: "manual" });
     const playLogin = await cookiefetch(schoolLogin.headers.get("location") || "", { redirect: "follow" });
     if (!playLogin.ok && !playLogin.status.toString().startsWith("3")) throw new Error(`Client ID request failed with a code of ${playLogin.status}`);
     if (log) console.log(`Client ID request done with a code of ${playLogin.status}.`);
     const clientId = (await playLogin.text()).match(/client_id="([0-9a-f]+)"/)?.[1];
     if (clientId === undefined) throw new Error("Client ID was not found on in the request response.");
     const tokenParams = new URLSearchParams();
+    const verifier = genRanHex(96);
+    const challenge = Buffer.from(sha256(verifier), "hex").toString("base64").replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
     tokenParams.set("client_id", clientId);
-    tokenParams.set("redirect_uri", "https://play.prodigygame.com/play");
-    tokenParams.set("response_type", "id_token token");
-    tokenParams.set("scope", "openid profile email sid identity_provider");
-    tokenParams.set("state", "b292a37841634f2eb2c6c283285e0e1a");
-    tokenParams.set("nonce", "e651b05312b74195beb22f99a116c630");
-    tokenParams.set("prompt", "login");
+    tokenParams.set("code_challenge", challenge);
+    tokenParams.set("code_challenge_method", "S256");
+    tokenParams.set("rid", rid as string);
+    tokenParams.set("redirect_uri", "https://play.prodigygame.com/launcher");
+    tokenParams.set("response_type", "code");
+    tokenParams.set("response_mode", "query");
+    tokenParams.set("scope", "openid profile email sid identity_provider premises");
+    tokenParams.set("state", "042a380791654df0b90a820cd940a799");
     tokenParams.set("mobilePlatform", "undefined");
     const tokenLogin = await cookiefetch("https://sso.prodigygame.com/oauth/authorize?" + tokenParams, {
         redirect: "manual"
     });
     if (!tokenLogin.ok && !tokenLogin.status.toString().startsWith("3")) throw new Error(`First authentication request failed with a code of ${tokenLogin.status}.`);
     if (log) console.log(`First token request done with a code of ${tokenLogin.status}.`);
-    const secondTokenLogin = await cookiefetch(tokenLogin.headers.get("location") || "", {
+    const launcherLogin = await cookiefetch(tokenLogin.headers.get("location") || "", {
         redirect: "manual"
     });
-    if (!secondTokenLogin.ok && !secondTokenLogin.status.toString().startsWith("3")) throw new Error(`Second authentication request failed with a code of ${secondTokenLogin.status}.`);
-    if (log) console.log(`Second token request done with a code of ${secondTokenLogin.status}.`);
-    const tokenProp = new URL((secondTokenLogin.headers.get("location") || "").replace("#", "?")).searchParams;
-    const tokenInit: TokenResponse = tokenProp.toString().split("&").reduce((obj, item) => {
-        obj[item.split("=")[0]] = decodeURIComponent(item.split("=")[1]);
-        return obj;
-    }, {} as {[key: string]: any}) as TokenResponse;
-    const master = await fetch("https://api.prodigygame.com/game-auth-api/v4/user", {
+    if (!launcherLogin.ok && !launcherLogin.status.toString().startsWith("3")) throw new Error(`Second authentication request failed with a code of ${launcherLogin.status}.`);
+    if (log) console.log(`Login request done with a code of ${launcherLogin.status}.`);
+    const tokenInfoParams = new URLSearchParams();
+    tokenInfoParams.set("client_id", clientId);
+    tokenInfoParams.set("code_verifier", verifier);
+    tokenInfoParams.set("grant_type", "authorization_code");
+    tokenInfoParams.set("redirect_uri", "https://play.prodigygame.com/launcher");
+    tokenInfoParams.set("code", new URL(launcherLogin.url).searchParams.get("code") as string);
+    const tokenInfo = await (await cookiefetch("https://sso.prodigygame.com/oauth/token", {
         headers: {
-            "Content-Type": "application/json"
+            accept: "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/x-www-form-urlencoded",
+            "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            Referer: "https://play.prodigygame.com/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
         },
+        body: tokenInfoParams.toString(),
+        method: "POST"
+    })).json();
+    const profileInfo = await (await cookiefetch("https://sso.prodigygame.com/oauth/userinfo", {
+        headers: {
+            accept: "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            authorization: "Bearer " + tokenInfo.access_token,
+            "if-none-match": "W/\"5b286425d5daec2d178cdc71cff23daa\"",
+            "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            Referer: "https://play.prodigygame.com/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        method: "GET"
+    })).json();
+    const decodedIdToken = JSON.parse(Buffer.from(tokenInfo.id_token.split(".")[1], "base64").toString());
+    await cookiefetch("https://api.prodigygame.com/game-cortex-server/v1/coreGame.setSession", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            identityToken: tokenInit.access_token
-        }),
+            identity_token: tokenInfo.access_token,
+            refresh_token: tokenInfo.refresh_token,
+            profile: {
+                ...profileInfo,
+                auth_time: decodedIdToken.auth_time,
+                sid: decodedIdToken.sid,
+                session_exp: decodedIdToken.session_exp
+            },
+            clientId: clientId,
+            identityUri: "https://sso.prodigygame.com"
+        })
+    });
+    const master = await cookiefetch("https://api.prodigygame.com/game-cortex-server/v1/coreGame.login", {
+        headers: {
+            accept: "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/json",
+            "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            Referer: "https://math.prodigygame.com/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        body: "{\"clientID\":\"1\"}",
         method: "POST"
     });
     if (!master.ok) throw new Error(`Master request failed with a code of ${master.status}.`);
     if (log) console.log(`Master request done with a code of ${master.status}.`);
     const masterJson: MasterResponse = await master.json();
-    return {
-        ...tokenInit,
-        ...masterJson
-    };
+    return masterJson;
 };
